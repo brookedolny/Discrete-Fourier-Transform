fab6f9fedcc4454e19d0ba34e07636c102ba1f5c
Brooke Dolny <brookedolny@gmail.com>
2018-06-10 00:05:40 -0400

    Made temp vectors slightly more memory efficient
    Allocated the correct amount of space at the beginning of the
    function instead of just using push_back()... lol

--- discreteFourier.cpp
+++ discreteFourier.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 void discreteFourierTransform(vector<complex<double> > & sequence) {
-    vector<complex<double> > transform;
+    vector<complex<double> > transform(sequence.size());
     complex<double> x;
     for(int k = 0; k < sequence.size(); k++) {
         x = 0;
@@ -12,7 +12,7 @@ void discreteFourierTransform(vector<complex<double> > & sequence) {
             x += sequence[n] * complex<double>(cos( (2 * M_PI * k * n) / sequence.size()), - sin( (2 * M_PI * k * n) / sequence.size()));
         }
         x /= sequence.size();
-        transform.push_back(x);
+        transform[k] = x;
     }
     for(int k = 0; k < sequence.size(); k++) {
         sequence[k] = transform[k];
@@ -20,14 +20,14 @@ void discreteFourierTransform(vector<complex<double> > & sequence) {
 }
 
 void inverseDiscreteFourierTransform(vector<complex<double> > & sequence) {
-    vector<complex<double> > transform;
+    vector<complex<double> > transform(sequence.size());
     complex<double> x;
     for(int n = 0; n < sequence.size(); n++) {
         x = 0;
         for(int k = 0; k < sequence.size(); k++) {
             x += sequence[k] * complex<double>(cos( (2 * M_PI * k * n) / sequence.size()), sin( (2 * M_PI * k * n) / sequence.size()));
         }
-        transform.push_back(x);
+        transform[n] = x;
     }
     for(int k = 0; k < sequence.size(); k++) {
         sequence[k] = transform[k];
