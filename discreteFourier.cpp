a8fce444142b6f2bffb8abc1f99eaa46ce3e5979
Brooke Dolny <brookedolny@gmail.com>
2018-06-09 21:16:52 -0400

    Initial Commit of Project

---
+++ discreteFourier.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <iostream>
+#include <complex>
+#include <cmath>
+
+using namespace std;
+
+void discreteFourierTransform(vector<complex<double> > & sequence) {
+    vector<complex<double> > transform;
+    complex<double> x;
+    for(int k = 0; k < sequence.size(); k++) {
+        x = 0;
+        for(int n = 0; n < sequence.size(); n++) {
+            x += sequence[n] * complex<double>(cos( (2 * M_PI * k * n) / sequence.size()), - sin( (2 * M_PI * k * n) / sequence.size()));
+        }
+        transform.push_back(x);
+    }
+    for(int k = 0; k < sequence.size(); k++) {
+        sequence[k] = transform[k];
+    }
+}
+
+void inverseDiscreteFourierTransform(vector<complex<double> > & sequence) {
+    vector<complex<double> > transform;
+    complex<double> x;
+    for(int n = 0; n < sequence.size(); n++) {
+        x = 0;
+        for(int k = 0; k < sequence.size(); k++) {
+            x += sequence[k] * complex<double>(cos( (2 * M_PI * k * n) / sequence.size()), + sin( (2 * M_PI * k * n) / sequence.size()));
+        }
+        x /= sequence.size();
+        transform.push_back(x);
+    }
+    for(int k = 0; k < sequence.size(); k++) {
+        sequence[k] = transform[k];
+    }
+}
