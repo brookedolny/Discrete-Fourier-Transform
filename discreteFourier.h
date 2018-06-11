3219ffaf93d9e15f0b14c0fd873de17c43cb343a
Brooke Dolny <brookedolny@gmail.com>
2018-06-10 22:41:50 -0400

    Added Documentation

--- discreteFourier.h
+++ discreteFourier.h
@@ -4,11 +4,20 @@
 #include <vector>
 #include <complex>
 
+/**
+ * Performs the Discrete Fourier Transform on a sequence of N complex numbers.
+ *
+ * @param sequence the N complex numbers to be transformed.
+ */
+void discreteFourierTransform(std::vector< std::complex<double> > & sequence);
 
-void discreteFourierTransform(std::vector<std::complex<double> > & sequence);
-
-
-void inverseDiscreteFourierTransform(std::vector<std::complex<double> > & sequence);
+/**
+ * Performs the inverse transformation of the Discrete Fourier Transform
+ * on a sequence of N complex numbers.
+ *
+ * @param sequence the N complex numbers to be transformed.
+ */
+void inverseDiscreteFourierTransform(std::vector< std::complex<double> > & sequence);
 
 
 #endif
